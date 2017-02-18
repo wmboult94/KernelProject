ed052e14b5ebda93368e56a94d1351be7fe0323d
Warren Boult <wmboult94@gmail.com>
2017-02-18 15:00:55 +0000

    Working initial front kernel

--- path_kernel.py
+++ path_kernel.py
@@ -1,40 +1,42 @@
 import numpy as np
 import math
+import timeit
 from sympy import Matrix
 import GPy as gpy
 import matplotlib.pyplot as plt
 from scipy.linalg import eigh
+from scipy.sparse import csr_matrix, kron
 from sklearn.decomposition import KernelPCA
 fac = math.factorial
 
 # generate some sequential datas
 pi = np.pi
-x = np.linspace(0,2*pi,50)
+x = np.linspace(0,2*pi,200)
 # y = np.linspace(0,2*pi,10)
 # x = x.reshape(20,1)
 cosx = np.array(np.cos(x))
 sinx = np.array(np.sin(x))
-cosx = cosx.reshape(50,1)
-sinx = sinx.reshape(50,1)
-noise1 = np.random.normal(0, 1, 50)
-noise1 = noise1.reshape(50,1)
+cosx = cosx.reshape(200,1)
+sinx = sinx.reshape(200,1)
+noise1 = np.random.normal(0, 1, 20)
+noise1 = noise1.reshape(20,1)
 noise2 = np.random.normal(0, 1, 20)
 noise2 = noise2.reshape(20,1)
 # sinnoisy = sinx + noise2
 # xnoisy2 = x + noise2
-y = np.linspace(0,100,50)
-y = y.reshape(50,1)
+y = np.linspace(0,100,20)
+y = y.reshape(20,1)
 
 cosines = []
 labels = []
 for i in range(0,10):
-	noise = np.random.normal(0, 0.15, 50)
+	noise = np.random.normal(0, 0.4, 200)
 	noise = cosx + noise
 	cosines.append(noise)
 	labels.append(0)
 
 for i in range(10,20):
-	noise = np.random.normal(0, 0.15, 50)
+	noise = np.random.normal(0, 0.4, 200)
 	noise = sinx + noise
 	cosines.append(noise)
 	labels.append(1)
@@ -49,10 +51,14 @@ def k_gamma(i, j, Chv, Cd):
 	return sum
 
 def strucKernel(Cd, Chv, xlen, ylen):
+	# tvalue = np.inf if t==0 else t * min(xlen, ylen) + 1	# similar method to the triangular kga, only compute values near to diagonal
 	result = np.zeros((xlen, ylen))
 	for i in range(1, xlen):
 		for j in range(1, ylen):
+			# if (i-j < tvalue and i-j > -tvalue):
 			result[i-1][j-1] = k_gamma(i, j, Chv, Cd)
+			# else:
+			# 	result[i-1][j-1] = 0
 	return result
 
 def pathKernel(s, t):
@@ -61,18 +67,29 @@ def pathKernel(s, t):
 	# tlen = np.linspace(0,len(t),len(t))
 	# slen = slen.reshape(len(s),1)
 	# tlen = tlen.reshape(len(t),1)
-	kronMatrix = np.kron(kernRBF.K(s,t),strucKernel(0.4,0.3,len(s),len(t)))
+	kronMatrix = kron(kernRBF.K(s,t),strucKernel(0.4,0.3,len(s),len(t)))
 	# plt.matshow(kronMatrix)
 	# plt.show()
 	total = kronMatrix.sum()
 	return total
+
+# Front kernel for sequences of same length, no overlap for now
+def frontKernel(s, t, f):
+	acc=1
+	result=0
+	for i in range(1, len(s), f):
+		result += acc * pathKernel(s[i-1:i-1+f], t[i-1:i-1+f])
+		# print(result)
+		acc += 1
+	return result
 
 def kernelPCA(X, y, n_components):
 
 	K = np.zeros((len(X),len(X)))
 	for i, x1 in enumerate(X):
 		for j, x2 in enumerate(X):
-			K[i][j] = pathKernel(x1,x2)
+			# K[i][j] = pathKernel(x1,x2)
+			K[i][j] = frontKernel(x1,x2,3)
 
 	plt.matshow(K)
 	# K = np.inner(cosines,cosines)
@@ -97,6 +114,19 @@ def kernelPCA(X, y, n_components):
 	plt.xlabel('PC1')
 	plt.ylabel('PC2')
 	plt.show()
+
+# functionality to time functions
+def wrapper(func, *args, **kwargs):
+	def wrapped():
+		return func(*args, **kwargs)
+	return wrapped
+
+# wrapped1 = wrapper(pathKernel, cosx, sinx)
+# wrapped2 = wrapper(frontKernel, cosx, sinx, 2)
+# wrapped3 = wrapper(kernelPCA,cosines,labels,2)
+# print('path kernel time: ', timeit.timeit(wrapped1))
+# print('front kernel time: ', timeit.timeit(wrapped2))
+# print('0.1 threshold val: ', timeit.timeit(wrapped3))
 
 # print(cosx)
 # print(cosines)
